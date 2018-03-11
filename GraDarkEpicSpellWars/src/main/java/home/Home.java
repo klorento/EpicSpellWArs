2bc435c08fc9c6a23fb21a3ac0c6e09bd450cd46
Kamil Lorentowicz <lorentoka@wp.pl>
2018-03-11 10:29:14 +0100

    Epic 2

--- GraDarkEpicSpellWars/src/main/java/home/Home.java
+++ GraDarkEpicSpellWars/src/main/java/home/Home.java
@@ -2,11 +2,19 @@ package home;
 
 import javax.swing.*;
 import javax.swing.JButton;
+import java.awt.*;
 
 public class Home extends JFrame {
     public Home() {
         super("Epic Spell Wars");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(700, 700);
+        setLocation(600,150);
+        setLayout(new FlowLayout());
+        add(new JButton("Choose the number of opponents")).createImage(100,100);
+        add(new JButton("Enter nickname")).createImage(100,100);
+        add(new JButton("Start Game")).createImage(100,100);
+        add(new JButton("Quit game")).createImage(100,100);
         setVisible(true);
     }
 }
